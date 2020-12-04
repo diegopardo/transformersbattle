7c59dbdedbdf8fa26276605cb334135a47a6bb48
Diego Pardo <diepardo@gmail.com>
2020-12-04 18:16:32 -0500

    API 19 SSL issue fix

--- app/src/main/java/com/diegopardo/transformersbattle/di/module/ApiModule.kt
+++ app/src/main/java/com/diegopardo/transformersbattle/di/module/ApiModule.kt
@@ -1,9 +1,10 @@
 package com.diegopardo.transformersbattle.di.module
 
 import com.diegopardo.transformersbattle.BuildConfig
+import com.diegopardo.transformersbattle.api.interceptor.AuthInterceptor
 import com.diegopardo.transformersbattle.api.service.AllSparkService
 import com.diegopardo.transformersbattle.api.service.TransformersService
-import com.diegopardo.transformersbattle.api.interceptor.AuthInterceptor
+import com.diegopardo.transformersbattle.api.util.TLSSocketFactory
 import com.diegopardo.transformersbattle.utils.BASE_URL
 import com.diegopardo.transformersbattle.utils.PreferencesHelper
 import com.facebook.stetho.okhttp3.StethoInterceptor
@@ -13,8 +14,12 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.security.KeyManagementException
+import java.security.KeyStoreException
+import java.security.NoSuchAlgorithmException
 import javax.inject.Named
 import javax.inject.Singleton
+
 
 @Module
 class ApiModule {
@@ -22,9 +27,26 @@ class ApiModule {
     @Singleton
     @Provides
     fun provideAllSparkRetrofit(): Retrofit {
+        val okHttpClientBuilder = OkHttpClient.Builder()
+
+        try {
+            val tlsSocketFactory = TLSSocketFactory()
+            if (tlsSocketFactory.trustManager != null) {
+                okHttpClientBuilder
+                    .sslSocketFactory(tlsSocketFactory, tlsSocketFactory.trustManager)
+            }
+        } catch (e: KeyManagementException) {
+            e.printStackTrace()
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        } catch (e: KeyStoreException) {
+            e.printStackTrace()
+        }
+
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(ScalarsConverterFactory.create())
+            .client(okHttpClientBuilder.build())
             .build()
     }
 
@@ -41,7 +63,26 @@ class ApiModule {
         preferencesHelper: PreferencesHelper,
         allSparkService: AllSparkService,
     ): Retrofit {
-        val okHttpClientBuilder = OkHttpClient.Builder().addInterceptor(AuthInterceptor(preferencesHelper, allSparkService))
+        val okHttpClientBuilder = OkHttpClient.Builder().addInterceptor(
+            AuthInterceptor(
+                preferencesHelper,
+                allSparkService
+            )
+        )
+
+        try {
+            val tlsSocketFactory = TLSSocketFactory()
+            if (tlsSocketFactory.trustManager != null) {
+                okHttpClientBuilder
+                    .sslSocketFactory(tlsSocketFactory, tlsSocketFactory.trustManager)
+            }
+        } catch (e: KeyManagementException) {
+            e.printStackTrace()
+        } catch (e: NoSuchAlgorithmException) {
+            e.printStackTrace()
+        } catch (e: KeyStoreException) {
+            e.printStackTrace()
+        }
 
         if (BuildConfig.DEBUG) {
             okHttpClientBuilder.apply {
