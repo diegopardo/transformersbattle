dbcca1dde071f7a7c8e51b8e6d7c06c509b210bd
Diego Pardo <diepardo@gmail.com>
2020-12-04 07:13:48 -0500

    Adding action bar action buttons

--- app/src/main/java/com/diegopardo/transformersbattle/ui/main/MainActivity.kt
+++ app/src/main/java/com/diegopardo/transformersbattle/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.diegopardo.transformersbattle.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.diegopardo.transformersbattle.R
 import com.diegopardo.transformersbattle.application.TransformersBattleApplication
 import com.diegopardo.transformersbattle.ui.fragment.TransformersFragment
@@ -17,5 +18,10 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, TransformersFragment.newInstance())
                     .commitNow()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
     }
 }
