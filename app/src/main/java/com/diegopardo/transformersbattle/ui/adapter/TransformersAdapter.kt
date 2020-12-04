b3f84e18a80620265ea88ebf423c0fd73aa6d907
Diego Pardo <diepardo@gmail.com>
2020-12-03 23:49:04 -0500

    Trying out Glide and Picasso to load the team icon: for some reason autobot team icon doesnt load.

--- app/src/main/java/com/diegopardo/transformersbattle/ui/adapter/TransformersAdapter.kt
+++ app/src/main/java/com/diegopardo/transformersbattle/ui/adapter/TransformersAdapter.kt
@@ -1,15 +1,19 @@
 package com.diegopardo.transformersbattle.ui.adapter
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.diegopardo.transformersbattle.databinding.RecyclerViewItemTransformerBinding
 import com.diegopardo.transformersbattle.model.pojo.Transformer
+import com.squareup.picasso.Picasso
 
 class TransformersAdapter(private val transformerList: MutableList<Transformer>) :
     RecyclerView.Adapter<TransformersAdapter.ViewHolder>() {
 
-    class ViewHolder(val binding: RecyclerViewItemTransformerBinding) : RecyclerView.ViewHolder(binding.root)
+    class ViewHolder(val binding: RecyclerViewItemTransformerBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val binding = RecyclerViewItemTransformerBinding.inflate(
@@ -22,6 +26,10 @@ class TransformersAdapter(private val transformerList: MutableList<Transformer>)
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val transformer = transformerList[position]
+        //Glide.with(viewHolder.binding.root.context).load(transformer.team_icon)
+        Glide.with(viewHolder.binding.root.context).load(transformer.team_icon)
+            .into(viewHolder.binding.transformerGroupIcon)
+        //Picasso.get().load(transformer.team_icon).into(viewHolder.binding.transformerGroupIcon)
         viewHolder.binding.transformerName.text = transformer.name
         viewHolder.binding.transformerOverallRating.text = transformer.getOverallRating().toString()
         viewHolder.binding.transformerStrength.text = transformer.strength.toString()
